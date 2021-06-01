583596015622b7f94f8cf6996f576cbe567be27d
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2021-06-01 21:46:21 +0200

    changed ssetup in index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+
+const router = express.Router();
 app.use(bodyParser.json());
+
+/*
 
 
 app.post('/hello', (req, res) => {  
   res.json('Hello back from Bogsynth');
 });
+
+module.exports = app;
+*/
+
+
+router.post('/hello', (req, res) => {  
+  res.json('Hello back from Bogsynth');
+});
+
+app.use('/.netlify/functions/api', router)
 
 module.exports = app;
 module.exports.handler = serverless(app);
